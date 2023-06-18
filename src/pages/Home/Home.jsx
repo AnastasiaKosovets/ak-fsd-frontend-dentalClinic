2dbfdb6bb58fbf54fd0404c4cf4b1e43cdabfdb6
Anastasia <anastasiakosovets@gmail.com>
2023-06-19 00:34:09 +0200

    feat: add carousel and information to home page, not responsive for now

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,9 @@ import './Home.css';
 // import { ChangeView } from '../../common/ChangeView/ChangeView';
 import { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import space1 from "../../img/space1.jpg";
+import space2 from "../../img/space2.jpg";
+import space3 from "../../img/space3.jpg";
  
 export const Home = () => {
 
@@ -16,39 +19,35 @@ export const Home = () => {
           <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src=""
+          className="d-block carouselImg"
+          src={space1}
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3 className="fw-semibold">Tú sonrisa es nuestra prioridad</h3>
+          {/* <p>Tú sonrisa es nuestra prioridad </p> */}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
+          className="d-block carouselImg"
+          src={space2}
           alt="Second slide"
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h3 className="fw-semibold">Contamos con el mejor equípo y últimas tecnologías</h3>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
+          className="d-block carouselImg"
+          src={space3}
           alt="Third slide"
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          <h3 className="fw-semibold">Trato especial para los mas peques</h3>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
