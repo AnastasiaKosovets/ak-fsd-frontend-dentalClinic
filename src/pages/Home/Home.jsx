3b4d0f7cf82a7090aaa763fb54894de91aabdbcf
Anastasia <anastasiakosovets@gmail.com>
2023-06-18 12:27:34 +0200

    feat: add images of clinic

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,57 @@
 import React from 'react';
 import './Home.css';
 import { ChangeView } from '../../common/ChangeView/ChangeView';
+import { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
  
 export const Home = () => {
+
+    const [index, setIndex] = useState(0);
+    const handleSelect = (selectedIndex) => {
+        setIndex(selectedIndex);
+      };
+
      return (
-         <div className='homeDesign'> Bienvenidos a nuestra Clínica Dental
+         <div className='homeDesign'> 
+          <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./img/ "
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Second slide&bg=282c34"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=20232a"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
             {/* <ChangeView 
                 path={"/login"}
                 name={"Login"}
