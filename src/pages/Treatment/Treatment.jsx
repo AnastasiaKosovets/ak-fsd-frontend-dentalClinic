082399e5d9f0a9f1b7d9016bb9539705c4aeae85
Anastasia <anastasiakosovets@gmail.com>
2023-06-21 16:24:30 +0200

    feat:add more img and modified cards of treatments

--- src/pages/Treatment/Treatment.jsx
+++ src/pages/Treatment/Treatment.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Treatment.css';
 import ortodonciaImg from "../../img/ortodonciaImg.jpg";
-import treatm5 from "../../img/treatm5.jpg";
+import treatm9 from "../../img/treatm9.jpg";
+import treatm7 from "../../img/treatm7.jpg";
+import treatm8 from "../../img/treatm8.jpg";
+import treatm11 from "../../img/treatm11.jpg";
 import treatm3a from "../../img/treatm3a.jpg";
-import treatm2 from "../../img/treatm2.jpg";
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import { Container } from "react-bootstrap";
@@ -20,7 +22,7 @@ export const Treatment = () => {
             <Card.Body>
               <Card.Title className='treatmentStyleName'>Ortodoncia</Card.Title>
               <Card.Title>
-              Cualquier persona puede solucionar problemas de maloclusión, malposición o apiñamientos dentales. Por consiguiente disponemos de una solución para cada persona y para cada edad.
+              Cualquier persona puede solucionar problemas de malposición o apiñamientos dentales. Por consiguiente disponemos de una solución para cada persona y su edad.
               </Card.Title>
             </Card.Body>
           </Card>
@@ -29,21 +31,53 @@ export const Treatment = () => {
           <Card className="m-2 mt-2" style={{ width: "25rem" }}>
             <Card.Img className="d-inline-block" variant="top" src={treatm3a} />
             <Card.Body>
-              <Card.Text>Lucia de Castro</Card.Text>
+              <Card.Title className='treatmentStyleName'>Endodoncia</Card.Title>
               <Card.Title>
-                Especialista en Odontopediatría de la Facultad de
-                Medicina y Odontología de Universitat de València
+              Se realiza con radiografía muy precisa cual nos permitirá ver tu grado de afectación, tiempo de trabajo y pasos a realizarte, comenzará el tratamiento
               </Card.Title>
             </Card.Body>
           </Card>
         </Col>
         <Col xs={12} sm={6} md={4} lg={4}>
-          <Card className="m-2 mt-2 mb-2" style={{ width: "20rem" }}>
-            {/* <Card.Img variant="top" src={doctor3} /> */}
+          <Card className="m-2 mt-2 mb-2" style={{ width: "25rem" }}>
+            <Card.Img variant="top" src={treatm9} />
             <Card.Body>
-              <Card.Text>Svetlana Razhevska</Card.Text>
+              <Card.Title className='treatmentStyleName'>Prótesis dentales</Card.Title>
               <Card.Title>
-                Especialista en Ortodoncia, trabaja con varios métodos como Invisalign
+              Nuestro equipo de protésicos dentales en Valencia te recomendará la que mejor se adapte a tus necesidades para que vuelvas a tener la sonrisa perfecta
+              </Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={12} sm={6} md={4} lg={4}>
+          <Card className="m-2 mt-2 mb-2" style={{ width: "25rem" }}>
+            <Card.Img variant="top" src={treatm11} />
+            <Card.Body>
+              <Card.Title className='treatmentStyleName'>Prevención Bucal</Card.Title>
+              <Card.Title>
+              La prevención en odontología como en cualquier área de medicina, es algo muy importante que debemos concienciarnos a realizarla por costumbre ya que es menos costosa económicamente y menos indolora
+              </Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={12} sm={6} md={4} lg={4}>
+          <Card className="m-2 mt-2 mb-2" style={{ width: "25rem" }}>
+            <Card.Img variant="top" src={treatm7} />
+            <Card.Body>
+              <Card.Title className='treatmentStyleName'>Odontopediatría</Card.Title>
+              <Card.Title>
+              En nuestro Centro Odontológico tenemos los mejores expertos en odontopediatría en Valencia, poniendo al cuidado, mantenimiento y prevención la salud dental del niño desde su infancia hasta su adolescencia
+              </Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={12} sm={6} md={4} lg={4}>
+          <Card className="m-2 mt-2 mb-2" style={{ width: "25rem" }}>
+            <Card.Img variant="top" src={treatm8} />
+            <Card.Body>
+              <Card.Title className='treatmentStyleName'>Empastes</Card.Title>
+              <Card.Title>
+              Los empastes dentales son un procedimiento odontológico para la eliminación de una caries devolviendo al diente su forma y color natural
               </Card.Title>
             </Card.Body>
           </Card>
