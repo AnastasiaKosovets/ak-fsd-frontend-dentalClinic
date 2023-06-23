d3737e2eae58ccb2fdfe953ea7dcc8385dde13d2
Anastasia <anastasiakosovets@gmail.com>
2023-06-23 16:52:03 +0200

    fix: delete grid from thisCard and resolve horizontal scroll

--- src/common/ProductCard/ProductCard.jsx
+++ src/common/ProductCard/ProductCard.jsx
@@ -28,7 +28,7 @@ export const ProductCard = ({
   const notFound = `https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png`;
   return (
     <div className="">
-      <Card className="productCardDesign" style={{ width: "19rem" }}>
+      <Card className="productCardDesign" style={{ width: "18rem" }}>
         <Card.Body>
           <Card.Text className="cardText">{id}</Card.Text>
           <Card.Text className="cardText">{email}</Card.Text>
