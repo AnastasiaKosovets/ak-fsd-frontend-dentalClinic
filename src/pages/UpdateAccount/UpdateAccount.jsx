8c950c85ee493d39525edade5af55217c09ff0d2
Anastasia <anastasiakosovets@gmail.com>
2023-06-26 23:19:59 +0200

    feat: create inputForm for updateAccount

--- src/pages/UpdateAccount/UpdateAccount.jsx
+++ src/pages/UpdateAccount/UpdateAccount.jsx
@@ -1,9 +1,54 @@
 import React from "react";
-import "./UpdateAccount.css"
+import "./UpdateAccount.css";
+import Form from "react-bootstrap/Form";
+import { Container } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export const UpdateAccount = () => {
-
-    return(
-        <div>Modifica tus datos</div>
-    )
-}
+  return (
+    <div className="updateDesign">
+      Modifica tus datos
+      <Container>
+        <Row className="justify-content-center">
+          <Col xs={10} md={6} lg={8}>
+            <Form>
+              <Form.Group className="mb-3" controlId="email">
+                <Form.Control type="email" placeholder="Email" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="password">
+                <Form.Control type="email" placeholder="Contraseña" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="firstName">
+                <Form.Control type="email" placeholder="Nombre" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="lastName">
+                <Form.Control type="email" placeholder="Apellido" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="document">
+                <Form.Control type="email" placeholder="DNI/NIE" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="dateOfBirth">
+                <Form.Control type="email" placeholder="Fecha de nacimiento" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="address">
+                <Form.Control type="email" placeholder="Dirección" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="telefonNumber">
+                <Form.Control type="email" placeholder="Número de teléfono" />
+              </Form.Group>
+            </Form>
+          </Col>
+          <Col xs={10} md={6} lg={6} className="d-flex justify-content-center">
+            <Link to="/" className="modInfo">
+              Confirmar
+            </Link>
+            <Link to="/account" className="modInfo">
+              Cancelar
+            </Link>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
