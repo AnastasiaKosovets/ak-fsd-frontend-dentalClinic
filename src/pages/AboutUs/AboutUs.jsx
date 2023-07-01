db3ccd7f3548dd9fdcf22ba81f9fd858390d6b54
Anastasia <anastasiakosovets@gmail.com>
2023-07-01 13:18:54 +0200

    refactor: Users view and AboutUs view

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -7,53 +7,58 @@ import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import { Container } from "react-bootstrap";
 import Col from "react-bootstrap/Col";
-// import { ChangeView } from "../../common/ChangeView/ChangeView";
 
 export const AboutUs = () => {
-  
   return (
-
     <div className="aboutDesign">
       <Container>
-      <Row>
-        <Col xs={10} sm={6} md={4} lg={4}>
-          <Card className="m-2 mt-2" style={{ width: "20rem" }}>
-            <Card.Img className="d-inline-block" variant="top" src={doctor1} />
-            <Card.Body>
-              <Card.Text>Dr. Juan Zubichi</Card.Text>
-              <Card.Title>
-                Especialista en Prótesis dentales, el mejor de la Facultad de
-                Medicina
-              </Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col xs={10} sm={6} md={4} lg={4}>
-          <Card className="m-2 mt-2" style={{ width: "20rem" }}>
-            <Card.Img className="d-inline-block" variant="top" src={doctor2mod} />
-            <Card.Body>
-              <Card.Text>Dra. Lucia de Castro</Card.Text>
-              <Card.Title>
-                Especialista en Odontopediatría de la Facultad de
-                Medicina y Odontología de Universitat de València
-              </Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col xs={12} sm={6} md={4} lg={4}>
-          <Card className="m-2 mt-2 mb-2" style={{ width: "20rem" }}>
-            <Card.Img variant="top" src={doctor3} />
-            <Card.Body>
-              <Card.Text>Dra. Svetlana Razhevska</Card.Text>
-              <Card.Title>
-                Especialista en Ortodoncia, trabaja con varios métodos como Invisalign
-              </Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+        <Row>
+          <Col xs={10} sm={6} md={4} lg={4}>
+            <Card className="m-2 mt-2" style={{ width: "20rem" }}>
+              <Card.Img
+                className="d-inline-block"
+                variant="top"
+                src={doctor1}
+              />
+              <Card.Body>
+                <Card.Text>Dr. Juan Zubichi</Card.Text>
+                <Card.Title>
+                  Especialista en Prótesis dentales, el mejor de su promoción de
+                  la Facultad de Medicina de Universitat de Valéncia
+                </Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={10} sm={6} md={4} lg={4}>
+            <Card className="m-2 mt-2" style={{ width: "20rem" }}>
+              <Card.Img
+                className="d-inline-block"
+                variant="top"
+                src={doctor2mod}
+              />
+              <Card.Body>
+                <Card.Text>Dra. Lucia de Castro</Card.Text>
+                <Card.Title>
+                  Especialista en Odontopediatría graduada en Medicina y
+                  Odontología de Universitat de València
+                </Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={12} sm={6} md={4} lg={4}>
+            <Card className="m-2 mt-2 mb-2" style={{ width: "20rem" }}>
+              <Card.Img variant="top" src={doctor3} />
+              <Card.Body>
+                <Card.Text>Dra. Svetlana Razhevska</Card.Text>
+                <Card.Title>
+                  Especialista en Ortodoncia, trabaja con varios métodos uno de
+                  ellos: Invisalign
+                </Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </Container>
-      
     </div>
   );
 };
--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,66 +1,51 @@
-import React, {useState, useEffect} from 'react';
-import './Users.css';
-// import { bringProducts } from '../../services/apiCalls';
-import { ProductCard } from '../../common/ProductCard/ProductCard';
-import { useSelector } from 'react-redux';
-import { userData } from '../userSlice';
-import { getAllUsersByAdmin } from '../../services/apiCalls';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "./Users.css";
+import { ProductCard } from "../../common/ProductCard/ProductCard";
+import { useSelector } from "react-redux";
+import { userData } from "../userSlice";
+import { getAllUsersByAdmin } from "../../services/apiCalls";
+import { useNavigate } from "react-router-dom";
 
 export const Users = () => {
+  // Redux hook, that permits access to dates of token
+  const credRdx = useSelector(userData);
+  const token = credRdx?.credentials?.token;
+  const [infoUser, setInfoUser] = useState([]);
+  const navigate = useNavigate();
 
-    const credRdx = useSelector(userData);
-    const token = credRdx?.credentials?.token;
-    const [infoUser, setInfoUser] = useState([]);
-    const navigate = useNavigate();
+  useEffect(() => {
+    getAllUsersByAdmin(token).then((res) => {
+      setInfoUser(res.data);
+    });
+  }, []);
 
-    useEffect(() => {
-        getAllUsersByAdmin(token).then((res) => {
-            setInfoUser(res.data);
-        });
-        // navigate("/");
-    }, []);
-
-    
-
-    return(
-        <div className='userDesign'>
-            {
-                infoUser.length > 0 
-                    ? (
-                        <div className="thisCard">
-                            {
-                                infoUser.map(
-                                    (infoUser) => {
-                                        return (
-                                            <div key={infoUser.id}>
-                                                <ProductCard className="usersCardDesign"
-                                                id={` Id: ${infoUser.id}`}
-                                                email={`Email: ${infoUser.email}`}
-                                                // password={`Contraseña: ${product.password}`}
-                                                firstName={`Nombre: ${infoUser.firstName}`}
-                                                lastName={`Apellido: ${infoUser.lastName}`}
-                                                document={`NIE/DNI: ${infoUser.document}`}
-                                                dateOfBirth={`Fecha de nacimiento: ${infoUser.dateOfBirth}`}
-                                                address={`Dirección: ${infoUser.address}`}
-                                                telefonNumber={`Número de teléfono: ${infoUser.telefonNumber}`}
-                                                collegialNumber={`Número de colegiado: ${infoUser.collegialNumber}`}
-                                                role_id={`Role_Id: ${infoUser.role_id}`}
-                                                />
-                                            </div>
-                                        )
-                                    }
-                                )
-                            }
-                        </div>
-                    )
-
-                    : (
-                        <div>CARGANDO...</div>
-                    )
-            }
-
-         </div>
-    )
-     
-}
+  return (
+    <div className="userDesign">
+      {infoUser.length > 0 ? (
+        <div className="thisCard">
+          {infoUser.map((infoUser) => {
+            return (
+              <div key={infoUser.id}>
+                <ProductCard
+                  className="usersCardDesign"
+                  id={` Id: ${infoUser.id}`}
+                  email={`Email: ${infoUser.email}`}
+                  firstName={`Nombre: ${infoUser.firstName}`}
+                  lastName={`Apellido: ${infoUser.lastName}`}
+                  document={`NIE/DNI: ${infoUser.document}`}
+                  dateOfBirth={`Fecha de nacimiento: ${infoUser.dateOfBirth}`}
+                  address={`Dirección: ${infoUser.address}`}
+                  telefonNumber={`Número de teléfono: ${infoUser.telefonNumber}`}
+                  collegialNumber={`Número de colegiado: ${infoUser.collegialNumber}`}
+                  role_id={`Role_Id: ${infoUser.role_id}`}
+                />
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div>CARGANDO...</div>
+      )}
+    </div>
+  );
+};
